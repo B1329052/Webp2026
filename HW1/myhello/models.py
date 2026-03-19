2bf93a259c0fbe23be37b255e729c5a12cd95bdf
B1329052 <B1329052@cgu.edu.tw>
2026-03-19 21:14:28 +0800

    建立model course

--- HW1/myhello/models.py
+++ HW1/myhello/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Course(models.Model):
+    department = models.CharField(max_length=100)
+    coursetitle = models.CharField(max_length=100)
+    instructor = models.CharField(max_length=100)
