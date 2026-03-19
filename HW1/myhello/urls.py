5ea10a69c90dade2a88801167691cc2a7949f0cc
B1329052 <B1329052@cgu.edu.tw>
2026-03-19 21:16:24 +0800

    修改myhello/urls.py

--- HW1/myhello/urls.py
+++ HW1/myhello/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HelloApiView.as_view(), name = 'index'),
+    path('addcourse', views.add_course, name = 'add_course'),
+    path('courselist', views.list_course, name = 'list_course'),
 ]
