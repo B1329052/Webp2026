2c467edee2e5ac2b4841f04ece96ac411da2c585
B1329052 <B1329052@cgu.edu.tw>
2026-05-06 15:51:00 +0800

    新增material kit

--- ex11/hello-world/src/cgu_multiButton.js
+++ ex11/hello-world/src/cgu_multiButton.js
@@ -1,11 +1,25 @@
-const changeText=(event)=>{
-  console.log(event.target)
-  event.target.innerText = event.target.innerText + "被點了"
-}
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AlarmIcon from '@mui/icons-material/Alarm';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
 const MultiButton=(num)=>{
   var output=[];
-  for(let i=1;i<num+1;++i)
-    output.push(<button onClick={changeText}>第{i}個按鍵</button>)
+  output.push(
+    <IconButton color="primary" aria-label="add to shopping cart">
+      <AddShoppingCartIcon />
+    </IconButton>
+  )
+  output.push(
+    <IconButton color="primary" aria-label="delete">
+      <DeleteIcon />
+    </IconButton>
+  )
+  output.push(
+    <IconButton color="primary" aria-label="add an alarm">
+      <AlarmIcon />
+    </IconButton>
+  )
   return output;
 }
 
